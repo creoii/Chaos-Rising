8f9b3c297c685c52ab59401993ab771916ab79f4
creoii <33011337+creoii@users.noreply.github.com>
2022-04-22 13:14:26 -0400

    github stashes suck

--- Assets/Scripts/Components/ProjectileGenerator.cs
+++ Assets/Scripts/Components/ProjectileGenerator.cs
@@ -25,7 +25,6 @@ public class ProjectileGenerator : MonoBehaviour
 
         for (int i = 0; i < attacks.Length; ++i)
         {
-            //                    += 90 ;
             attacks[i].startAngle += 90;
             for (int j = 0; j < attacks[i].projectileCount; ++j)
             {
